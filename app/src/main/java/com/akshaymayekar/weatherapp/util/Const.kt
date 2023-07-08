6301b7e291d6cb2ce1646eb58bbcc3be544264dd
AkshayMayekarAndroid <56432623+akshayhungama@users.noreply.github.com>
2023-07-08 21:05:13 +0530

    project push

--- app/src/main/java/com/akshaymayekar/weatherapp/util/Const.kt
+++ app/src/main/java/com/akshaymayekar/weatherapp/util/Const.kt
@@ -2,15 +2,6 @@ package com.akshaymayekar.weatherapp.util
 
 object Const {
 
-
-    const val HOME_SCREEN = "home"
-    const val DETAIL_SCREEN = "detail"
-    const val DETAIL_ARG_GAMES_ID = "gamesId"
-    const val TITLE = "title"
-    const val IMAGE = "image"
-    const val RELEASE_DATE = "releaseDate"
-    const val OVERVIEW = "overview"
-
     const val KEY_API = "put your own key"
     const val WEB_API = "https://api.openweathermap.org/"
 }
